02306cefd36dfb2c74bf2b0ef057a4e560fa9a69
Rajan Singh ✅ <145154389+versatilerajan@users.noreply.github.com>
2025-11-06 12:33:05 +0530

    Fix import case sensitivity for components

--- spike/src/App.jsx
+++ spike/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Login from './components/login.jsx';
-import Chat from './components/chat.jsx';
+import Login from './components/Login.jsx';
+import Chat from './components/Chat.jsx';
 
 function App() {
   const [username, setUsername] = useState(null);
